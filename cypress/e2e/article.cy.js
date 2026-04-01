317c56e895bc12a02f11064bd685362ee10c01af
Igor <igor.olshanskyi@gmail.com>
2026-04-01 18:26:36 +0300

    fix

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -3,10 +3,6 @@ import { faker } from '@faker-js/faker';
 
 describe('Article Flow', () => {
   let user;
-  const articleTitle = faker.lorem.sentence();
-  const articleDescription = faker.lorem.sentence();
-  const articleBody = faker.lorem.paragraphs(1);
-
   before(() => {
     cy.task('generateUser').then((generatedUser) => {
       user = generatedUser;
@@ -16,25 +12,28 @@ describe('Article Flow', () => {
   });
 
   it('should create a new article', () => {
+    const articleTitle = faker.lorem.sentence();
+    const articleDescription = faker.lorem.sentence();
+    const articleBody = faker.lorem.paragraphs(1);
     cy.get('a[href="/editor"]').click();
     cy.get('input[placeholder="Article Title"]').type(articleTitle);
     cy.get('input[placeholder="What\'s this article about?"]')
       .type(articleDescription);
-    cy.get('input[placeholder="Write your article (in markdown"]')
+    cy.get('textarea[placeholder="Write your article (in markdown)"]')
       .type(articleBody);
-    cy.get('button[type="button"]').contains('Publish Artical').click();
+    cy.get('button[type="button"]').contains('Publish Articale').click();
     cy.get('h1').should('contain', articleTitle);
-    cy.get('.artical-content').should('contain', articleBody);
+    cy.get('.article-content').should('contain', articleBody);
   });
 
-  it('should delete an existing artical', () => {
+  it('should delete an existing article', () => {
     const titleToDelete = 'Delete Me ' + faker.string.uuid();
     cy.createArticle(titleToDelete, 'Desc', 'Body');
     cy.visit(`/@${user.username}`);
     cy.contains('h1', titleToDelete).click();
     cy.get('.btn-outline-danger')
-      .contains('Delete Articale').first().click();
+      .contains('Delete Article').first().click();
     cy.visit(`/@${user.username}`);
-    cy.contains(titleToDelete).should('not exist');
+    cy.contains(titleToDelete).should('not.exist');
   });
 });
